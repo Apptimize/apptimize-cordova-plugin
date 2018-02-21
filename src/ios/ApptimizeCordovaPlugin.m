7d5fabb300ffc182695d1f0af2cbb1a33a863ef2
Yinmeng Zhang <yinmeng.zhang@gmail.com>
2018-02-21 14:23:06 -0800

    Dv ff methods (#5)
    * Added methods for dynamic variables, feature flags and testInfo
    
    * Syntax changes based on feedback
    
    * Additional syntax changes
    
    * Additional syntax changes
    
    * removing testinfo from header file

--- src/ios/ApptimizeCordovaPlugin.m
+++ src/ios/ApptimizeCordovaPlugin.m
@@ -1,12 +1,13 @@
 #import "ApptimizeCordovaPlugin.h"
 #import <Cordova/CDVPlugin.h>
 #import <Apptimize/Apptimize.h>
+#import <Apptimize/Apptimize+Variables.h>
 
 @implementation ApptimizeCordovaPlugin
 
 - (void)pluginInitialize {
     [super pluginInitialize];
-    
+
     NSDictionary *settings = self.commandDelegate.settings;
     NSString *appKey = settings[@"com.apptimize.ios.app_key"];
     [Apptimize startApptimizeWithApplicationKey:appKey];
@@ -20,39 +21,61 @@
 }
 */
 
-- (void)runTest:(CDVInvokedUrlCommand*)command
+- (void)runTest:(nonnull CDVInvokedUrlCommand*)command
 {
-    NSString* callbackId = [command callbackId];
-    NSString* testName = [command argumentAtIndex:0];
-    NSArray* variationNames = [command argumentAtIndex:1];
+    NSString *callbackId = [command callbackId];
+    NSString *testName = [command argumentAtIndex:0];
+    NSArray *variationNames = [command argumentAtIndex:1];
 
     NSMutableArray *apptimizeCodeBlocks = [NSMutableArray new];
     for ( int i = 0; i < variationNames.count; i++ ) {
         ApptimizeCodeBlock *apptimizeCodeBlock = [[ApptimizeCodeBlock alloc] initWithName:variationNames[i]
                                                                                  andBlock:^()
         {
-            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:i+1];
+            CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:i+1];
             [self.commandDelegate sendPluginResult:result callbackId:callbackId];
         }];
         [apptimizeCodeBlocks addObject:apptimizeCodeBlock];
     }
     [Apptimize runTest:testName withBaseline:^() {
-        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:0];
+        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:0];
         [self.commandDelegate sendPluginResult:result callbackId:callbackId];
     } andApptimizeCodeBlocks:apptimizeCodeBlocks];
 }
 
 - (void)track:(CDVInvokedUrlCommand*)command
 {
-    NSString* eventName = [command argumentAtIndex:0];
+    NSString *eventName = [command argumentAtIndex:0];
     [Apptimize track:eventName];
 }
 
 - (void)trackValue:(CDVInvokedUrlCommand*)command
 {
-    NSString* eventName = [command argumentAtIndex:0];
-    NSNumber* eventValue = [command argumentAtIndex:1];
+    NSString *eventName = [command argumentAtIndex:0];
+    NSNumber *eventValue = [command argumentAtIndex:1];
     [Apptimize track:eventName value:[eventValue doubleValue]];
+}
+
+- (void)isFeatureFlagOn:(CDVInvokedUrlCommand*)command
+{
+    NSString *name = [command argumentAtIndex:0];
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:[Apptimize isFeatureFlagOn:name]];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)integerWithName:(CDVInvokedUrlCommand*)command
+{
+    NSString *name = [command argumentAtIndex:0];
+    NSNumber *defaultInteger = [command argumentAtIndex:1];
+    NSInteger value = [defaultInteger integerValue];
+    [ApptimizeVariable integerWithName:name andDefaultInteger:value];
+}
+
+- (void)stringWithName:(CDVInvokedUrlCommand*)command
+{
+    NSString *name = [command argumentAtIndex:0];
+    NSString *defaultString = [command argumentAtIndex:1];
+    [ApptimizeVariable stringWithName:name andDefaultString:defaultString];
 }
 
 @end
