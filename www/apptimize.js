c11b5ddb8a0ea5b4be6202b3de11b7ff7b543218
Vishal Chandnani <vishal.chandnani@sv.cmu.edu>
2018-02-21 13:56:53 -0800

    Added methods for dynamic variables, feature flags and testInfo (#3)
    * Added methods for dynamic variables, feature flags and testInfo
    
    * Syntax changes based on feedback
    
    * Additional syntax changes
    
    * Additional syntax changes
    
    * removing testinfo from header file

--- www/apptimize.js
+++ www/apptimize.js
@@ -4,7 +4,7 @@ var Apptimize = function () {
 /* Apptimize start is done automatically and the appkey is set via the config.xml preference */
 /*
 Apptimize.prototype.start = function(appKey) {
-    cordova.exec(function(success){}, function(err) {}, "ApptimizeCordovaPlugin", "startApptimize", [appKey]);
+    cordova.exec(function(success){}, function(error) {}, "ApptimizeCordovaPlugin", "startApptimize", [appKey]);
 };
 */
 
@@ -16,15 +16,29 @@ Apptimize.prototype.runTest = function(testName, baseline, variationNames, varia
         else {
             variationBlocks[codeblockNumber-1]();
         }
-    }, function(err) {}, "ApptimizeCordovaPlugin", "runTest", [testName, variationNames]);
+    }, function(error) {}, "ApptimizeCordovaPlugin", "runTest", [testName, variationNames]);
 };
 
 Apptimize.prototype.track = function(eventName) {
-    cordova.exec(function(suc){}, function(err) {}, "ApptimizeCordovaPlugin", "track", [eventName]);
+    cordova.exec(function(success){}, function(error) {}, "ApptimizeCordovaPlugin", "track", [eventName]);
 };
 
 Apptimize.prototype.trackValue = function(eventName, value) {
-    cordova.exec(function(suc){}, function(err) {}, "ApptimizeCordovaPlugin", "trackValue", [eventName, value]);
+    cordova.exec(function(success){}, function(error) {}, "ApptimizeCordovaPlugin", "trackValue", [eventName, value]);
+};
+
+Apptimize.prototype.isFeatureFlagOn = function(name) {
+    cordova.exec(function(success){
+        alert(success);
+    }, function(error) {}, "ApptimizeCordovaPlugin", "isFeatureFlagOn", [name]);
+};
+
+Apptimize.prototype.integerWithName = function(name, defaultInteger) {
+    cordova.exec(function(success){}, function(error) {}, "ApptimizeCordovaPlugin", "integerWithName", [name, defaultInteger]);
+};
+
+Apptimize.prototype.stringWithName = function(name, defaultString) {
+    cordova.exec(function(success){}, function(error) {}, "ApptimizeCordovaPlugin", "stringWithName", [name, defaultString]);
 };
 
 module.exports = new Apptimize();
